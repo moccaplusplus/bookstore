27c31fa5b0c7d1b446faa5c6059145e1496b713e
Tomasz Gawel <tomaszgawel@op.pl>
2022-08-25 12:41:53 +0200

    Initial commit

---
+++ src/test/java/pl/szkolaspringa/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szkolaspringa.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
